205a341b59a94244f13da7388b7cf9b994ba89a7
mordecai-ooh <guilherme.alves07@escola.pr.gov.br>
2025-03-12 10:48:58 -0300

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main(){
+   float c, f;
+   
+    scanf("%f" , &c);
+    f = (c * 1.8) + 32;
+    printf(" F = %.2f, c = %.0f" , f, c);
+     
+    return 0;
+
+
+    
+    
+}
